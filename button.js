306949b0fabecbf66aeae9d81911e9374c61fbba
Tyler Janzen <tylerjanzen04@gmail.com>
2024-03-30 21:59:05 -0400

    Add files via upload

---
+++ button.js
@@ -0,0 +1,11 @@
+//js for buttons
+
+var navLinks = document.getElementById("navLinks");
+
+function closeMenu(){
+    navLinks.style.right = "-200px";
+}
+
+function openMenu(){
+    navLinks.style.right = "0";
+}
